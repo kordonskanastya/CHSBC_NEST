fd009143944e27ff1852603d215006733cf9dea3
Vladimir Sergeichik <volodymyr.serheichyk.triare@gmail.com>
2022-05-24 11:47:01 +0300

    test CI

--- src/swagger.ts
+++ src/swagger.ts
@@ -2,7 +2,7 @@ import { INestApplication } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 export default function setupSwagger(app: INestApplication) {
-  const config = new DocumentBuilder().setTitle('CSBC API').setVersion('1.0.0').addBearerAuth().build()
+  const config = new DocumentBuilder().setTitle('CSBC API').setVersion('1.0.0 (test CI)').addBearerAuth().build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('swagger', app, document, {
     swaggerOptions: {

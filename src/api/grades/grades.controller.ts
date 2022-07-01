f4e6d71b282c61dcc602722fdc965181e8aaeb3b
OleksandrKorzh1 <oleksandrkorzh.dev@gmail.com>
2022-07-01 19:50:08 +0200

    added migration  for grades ,added create grade

--- src/api/grades/grades.controller.ts
+++ src/api/grades/grades.controller.ts
@@ -1,34 +1,58 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common'
 import { GradesService } from './grades.service'
 import { CreateGradeDto } from './dto/create-grade.dto'
 import { UpdateGradeDto } from './dto/update-grade.dto'
+import { MinRole } from '../../auth/roles/roles.decorator'
+import { ROLE } from '../../auth/roles/role.enum'
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger'
+import { CreateGradeResponseDto } from './dto/create-grade-response.dto'
+import { Entities } from '../common/enums'
+import { capitalize } from '../../utils/common'
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard'
+import { RolesGuard } from '../../auth/roles/roles.guard'
 
-@Controller('grades')
+@Controller(Entities.GRADES)
+@ApiTags(capitalize(Entities.GRADES))
+@ApiBearerAuth()
+@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+@ApiForbiddenResponse({ description: 'Forbidden resource. Check user role' })
+@MinRole(ROLE.TEACHER)
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class GradesController {
   constructor(private readonly gradesService: GradesService) {}
 
   @Post()
-  create(@Body() createGradeDto: CreateGradeDto) {
-    return this.gradesService.create(createGradeDto)
+  @MinRole(ROLE.TEACHER)
+  @ApiCreatedResponse({ type: CreateGradeResponseDto })
+  @ApiBadRequestResponse({ description: 'Bad request' })
+  async create(@Body() createGradeDto: CreateGradeDto): Promise<CreateGradeResponseDto> {
+    return await this.gradesService.create(createGradeDto)
   }
 
   @Get()
-  findAll() {
-    return this.gradesService.findAll()
+  async findAll() {
+    return await this.gradesService.findAll()
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.gradesService.findOne(+id)
+  async findOne(@Param('id') id: string) {
+    return await this.gradesService.findOne(+id)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGradeDto: UpdateGradeDto) {
-    return this.gradesService.update(+id, updateGradeDto)
+  async update(@Param('id') id: string, @Body() updateGradeDto: UpdateGradeDto) {
+    return await this.gradesService.update(+id, updateGradeDto)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.gradesService.remove(+id)
+  async remove(@Param('id') id: string) {
+    return await this.gradesService.remove(+id)
   }
 }
--- src/api/grades/grades.service.ts
+++ src/api/grades/grades.service.ts
@@ -1,26 +1,61 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Inject, Injectable } from '@nestjs/common'
 import { CreateGradeDto } from './dto/create-grade.dto'
 import { UpdateGradeDto } from './dto/update-grade.dto'
+import { GRADE_REPOSITORY } from '../../constants'
+import { Repository } from 'typeorm'
+import { Grade } from './entities/grade.entity'
+import { plainToClass } from 'class-transformer'
+import { CreateGroupResponseDto } from '../groups/dto/create-group-response.dto'
+import { TokenDto } from '../../auth/dto/token.dto'
+import { Course } from '../courses/entities/course.entity'
+import { Student } from '../students/entities/student.entity'
 
 @Injectable()
 export class GradesService {
-  create(createGradeDto: CreateGradeDto) {
-    return 'This action adds a new grade'
+  constructor(
+    @Inject(GRADE_REPOSITORY)
+    private gradeRepository: Repository<Grade>,
+  ) {}
+
+  async create(createGradeDto: CreateGradeDto, tokenDto?: TokenDto) {
+    const { sub } = tokenDto || {}
+    const student = await Student.findOne(createGradeDto.studentId)
+    const course = await Course.findOne(createGradeDto.courseId)
+
+    if (!student) {
+      throw new BadRequestException(`This student id: ${createGradeDto.studentId} not found.`)
+    }
+
+    if (!course) {
+      throw new BadRequestException(`This course id: ${createGradeDto.courseId} not found.`)
+    }
+
+    const grade = await this.gradeRepository
+      .create({
+        ...createGradeDto,
+        student,
+        course,
+      })
+      .save({ data: { id: sub } })
+
+    return plainToClass(CreateGroupResponseDto, grade, {
+      excludeExtraneousValues: true,
+    })
   }
 
-  findAll() {
-    return `This action returns all grades`
+  async findAll() {
+    return await `This action returns all grades`
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} grade`
+  async findOne(id: number) {
+    return await `This action returns a #${id} grade`
   }
 
-  update(id: number, updateGradeDto: UpdateGradeDto) {
-    return `This action updates a #${id} grade`
+  async update(id: number, updateGradeDto: UpdateGradeDto) {
+    return await `This action updates a #${id} grade`
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} grade`
+  async remove(id: number) {
+    return await `This action removes a #${id} grade`
   }
 }

7b82337546e0794d4ca2c28a706f792139c77424
OleksandrKorzh1 <oleksandrkorzh.dev@gmail.com>
2022-08-27 16:09:54 +0200

    created curator role

--- src/api/users/dto/create-teacher.dto.ts
+++
@@ -1,9 +0,0 @@
-import { CreateUserDto } from './create-user.dto'
-import { ApiProperty, PartialType } from '@nestjs/swagger'
-import { IsArray } from 'class-validator'
-
-export class CreateTeacherDto extends PartialType(CreateUserDto) {
-  @IsArray()
-  @ApiProperty({ required: false, example: [43, 12] })
-  courses: number[]
-}
--- src/api/users/users.service.ts
+++ src/api/users/users.service.ts
@@ -29,6 +29,7 @@ import { GetUserDropdownResponseDto } from './dto/get-user-dropdown-response.dto
 import { ROLE } from '../../auth/roles/role.enum'
 import { GetGroupsByCuratorDto } from './dto/get-groups-by-curator.dto'
 import { GetCoursesByTeacherDto } from './dto/get-courses-by-teacher.dto'
+import { GetCuratorInfoDto } from './dto/get-curator-info.dto'
 
 export enum UserColumns {
   ID = 'id',
@@ -429,5 +430,19 @@ export class UsersService {
 
     query.orderBy(`User.${orderByColumn}`, orderBy)
     return await paginateAndPlainToClass(GetCoursesByTeacherDto, query, options)
+  }
+
+  async getCuratorInfo(token: TokenDto) {
+    const { sub } = token || {}
+    const curatorInfo = await this.usersRepository
+      .createQueryBuilder('User')
+      .leftJoinAndSelect('User.groups', 'Group')
+      .leftJoinAndSelect('Group.students', 'Student')
+      .leftJoinAndSelect('Student.grades', 'Grade')
+      .leftJoinAndSelect('Grade.course', 'Course')
+      .leftJoinAndSelect('Student.user', 'User_')
+      .where('User.id=:id', { id: sub })
+      .getOne()
+    return plainToClass(GetCuratorInfoDto, curatorInfo, { excludeExtraneousValues: true })
   }
 }

6227e40c5b52caa06dfc6b448585a89e5624e4c9
OleksandrKorzh1 <oleksandrkorzh.dev@gmail.com>
2022-10-01 19:01:07 +0200

    rename dto

--- src/api/voting/dto/get-voting-result.dto.ts
+++ src/api/voting/dto/get-voting-result.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Expose, Type } from 'class-transformer'
-import { GetCourseTeacherDto } from '../../courses/dto/get-course-teacher.dto'
+import { GetCourseForVotingResultDto } from '../../courses/dto/get-course-for-voting-result.dto'
 import { CreateGroupResponseDto } from '../../groups/dto/create-group-response.dto'
 import { GetStudentForVotingResult } from '../../students/dto/get-student-for-voting-result'
 
@@ -31,6 +31,6 @@ export class GetVotingResultDto {
   students: GetStudentForVotingResult
 
   @Expose()
-  @Type(() => GetCourseTeacherDto)
-  courses: GetCourseTeacherDto
+  @Type(() => GetCourseForVotingResultDto)
+  courses: GetCourseForVotingResultDto
 }
